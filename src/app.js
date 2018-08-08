7e6a461757d2dc0e0396bcaa1a861938b1f11acf
awchjimmy <awchjimmy@gmail.com>
2018-08-09 00:57:43 +0800

    fish can move to specific point

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
   render: render
 });
 var cursors
+var tropicalfish1
 
 function preload() {
   game.load.image('bg1', 'http://127.0.0.1:8887/assets/bg1.jpg')
@@ -12,16 +13,23 @@ function preload() {
 }
 
 function create() {
+  game.physics.startSystem(Phaser.Physics.ARCADE)
   game.add.image(0, 0, 'bg1')
-
-  let tropicalfish1 = game.add.image(500, 300, 'tropicalfish')
-  tropicalfish1.scale.setTo(0.3, 0.3)
-
   game.world.setBounds(0, 0, 1000, 526)
   cursors = game.input.keyboard.createCursorKeys()
+
+
+  tropicalfish1 = game.add.sprite(500, 300, 'tropicalfish')
+  tropicalfish1.scale.setTo(0.3, 0.3)
+  tropicalfish1.anchor.setTo(0, 0.5)
+  game.physics.enable(tropicalfish1, Phaser.Physics.ARCADE);
+  tropicalfish1.body.allowRotation = false
+
 }
 
 function update() {
+  game.physics.arcade.moveToXY(tropicalfish1, 100, 100, 60, 1500)
+
   if (cursors.up.isDown) {
     game.camera.y -= 4
   }
@@ -37,5 +45,6 @@ function update() {
 }
 
 function render() {
-  game.debug.cameraInfo(game.camera, 32, 32);
+  // game.debug.cameraInfo(game.camera, 32, 32)
+  game.debug.spriteInfo(tropicalfish1, 32, 32)
 }
