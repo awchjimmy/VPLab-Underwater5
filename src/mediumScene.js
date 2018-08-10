6b7889ad22c9c38b2015e304e46a249132048fbf
awchjimmy <awchjimmy@gmail.com>
2018-08-10 21:52:27 +0800

    createFishCollection, everything is on the right track

--- src/mediumScene.js
+++ src/mediumScene.js
@@ -4,14 +4,15 @@
  * Praise the Lord.
  */
 
-var mediumState = function(game) {}
+var mediumState = function (game) { }
 
 mediumState.prototype = {
   // vars
+  fishCount: 3,
   fishCollection: [],
 
   // funcs
-  preload: function() {
+  preload: function () {
     this.game.load.image('bg2', './assets/bg2.jpg')
 
     this.game.load.image('fish101', './assets/fish101.png')
@@ -24,15 +25,9 @@ mediumState.prototype = {
 
     this.game.add.image(0, 0, 'bg2')
 
-    let whale = this.game.add.sprite(100, 200, 'fish101')
-    this.game.physics.enable(whale, Phaser.Physics.ARCADE) // after enable physics, there is sprite.body, kinda cool
-    // console.log(whale)
-    whale.scale.set(0.5, 0.5)
-    whale.body.velocity.set(100, 0)
-
-    this.fishCollection.push(whale)
 
     // console.log(this.fishCollection)
+    this.createFishCollection()
   },
 
   update: function () {
@@ -41,5 +36,18 @@ mediumState.prototype = {
 
   render: function () {
 
+  },
+
+  createFishCollection: function () {
+    for (let i = 0; i < this.fishCount; i++) {
+      let whale = this.game.add.sprite(100, 200, 'fish101')
+      this.game.physics.enable(whale, Phaser.Physics.ARCADE) // after enable physics, there is sprite.body, kinda cool
+      // console.log(whale)
+
+      whale.scale.set(_.random(0.3, 0.5, true))
+      whale.body.velocity.set(100, _.random(-15, 15))
+
+      this.fishCollection.push(whale)
+    }
   }
 }
