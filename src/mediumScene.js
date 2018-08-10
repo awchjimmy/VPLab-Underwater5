b7421bc6fd162f05bfe51a5ba4363bc6c1ebed96
awchjimmy <awchjimmy@gmail.com>
2018-08-11 01:07:12 +0800

    add oop camera to mediumScene

--- src/mediumScene.js
+++ src/mediumScene.js
@@ -26,6 +26,7 @@ mediumState.prototype = {
   create: function () {
     // game engine settings
     this.game.physics.startSystem(Phaser.Physics.ARCADE)
+    this.initCamera()
 
 
     this.game.add.image(0, 0, 'bg2')
@@ -36,7 +37,7 @@ mediumState.prototype = {
   },
 
   update: function () {
-    updateCameraViaKeyboard()
+    this.updateCameraViaKeyboard()
   },
 
   render: function () {
@@ -47,7 +48,7 @@ mediumState.prototype = {
     for (let i = 0; i < this.fishCount; i++) {
       let x = _.random(this.width)
       let y = _.random(this.height)
-      let rndKeyname = this.fishKeyCollction[_.random(this.fishKeyCollction.length-1)]
+      let rndKeyname = this.fishKeyCollction[_.random(this.fishKeyCollction.length - 1)]
       let whale = this.game.add.sprite(x, y, rndKeyname)
       this.game.physics.enable(whale, Phaser.Physics.ARCADE) // after enable physics, there is sprite.body, kinda cool
       // console.log(whale)
@@ -57,5 +58,26 @@ mediumState.prototype = {
 
       this.fishCollection.push(whale)
     }
+  },
+
+  initCamera: function () {
+    // // set bounds for camera
+    this.game.world.setBounds(0, 0, width, height)
+    // // set keyboard for camera control
+    this.cursors = this.game.input.keyboard.createCursorKeys()
+  },
+
+  updateCameraViaKeyboard: function () {
+    if (this.cursors.up.isDown) {
+      this.game.state.start('Shallow')
+    } else if (this.cursors.down.isDown) {
+      this.game.state.start('Medium')
+    }
+    if (this.cursors.left.isDown) {
+      this.game.camera.x -= 4
+    } else if (this.cursors.right.isDown) {
+      this.game.camera.x += 4
+    }
   }
+
 }
