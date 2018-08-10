eb3527acf39812b8f3210bb7912ae151719ae37d
awchjimmy <awchjimmy@gmail.com>
2018-08-10 23:49:36 +0800

    tune createFishCollection

--- src/mediumScene.js
+++ src/mediumScene.js
@@ -8,14 +8,19 @@ var mediumState = function (game) { }
 
 mediumState.prototype = {
   // vars
-  fishCount: 3,
+  width: 5000,
+  height: 600,
+  fishCount: 10,
   fishCollection: [],
+  fishKeyCollction: ['fish101', 'fish102', 'fish103'],
 
   // funcs
   preload: function () {
     this.game.load.image('bg2', './assets/bg2.jpg')
 
     this.game.load.image('fish101', './assets/fish101.png')
+    this.game.load.image('fish102', './assets/fish102.png')
+    this.game.load.image('fish103', './assets/fish103.png')
   },
 
   create: function () {
@@ -40,7 +45,10 @@ mediumState.prototype = {
 
   createFishCollection: function () {
     for (let i = 0; i < this.fishCount; i++) {
-      let whale = this.game.add.sprite(100, 200, 'fish101')
+      let x = _.random(this.width)
+      let y = _.random(this.height)
+      let rndKeyname = this.fishKeyCollction[_.random(this.fishKeyCollction.length-1)]
+      let whale = this.game.add.sprite(x, y, rndKeyname)
       this.game.physics.enable(whale, Phaser.Physics.ARCADE) // after enable physics, there is sprite.body, kinda cool
       // console.log(whale)
 
