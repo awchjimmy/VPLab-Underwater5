ff2a203ecdfa1db4d6214dce72fcc7673d040efb
awchjimmy <awchjimmy@gmail.com>
2018-08-17 23:52:09 +0800

    add sub bgm

--- src/subdivingScene.js
+++ src/subdivingScene.js
@@ -14,18 +14,19 @@ subdivingState.prototype = {
   preload: function () {
 
     this.game.load.spritesheet('sp_sub', './assets/sp_sub.png', 800, 600, 65)
-    this.game.load.audio('bgm3', './assets/bgm3.mp3', true)
+    this.game.load.audio('bgm', './assets/sub.mp3', true)
   },
 
   create: function () {
 
     // bgm
-    this.bgm = this.game.add.audio('bgm3', 1, true)
+    this.bgm = this.game.add.audio('bgm', 1, false)
     this.bgm.play()
 
     let sub = this.game.add.sprite(0, 0, 'sp_sub')
     let anim = sub.animations.add('diving')
     anim.onComplete.add(function(){
+      this.bgm.stop()
       this.game.state.start('Medium')
     }, this)
     anim.play(10, false)
